3e9ec8df4f309d60c0e52ae499e3caaeed9497c7
GGabi40 <gabriela.bc2000@hotmail.com>
2024-04-11 20:40:27 -0300

    updating project

---
+++ script.js
@@ -0,0 +1,15 @@
+/* Dados DOM */
+const form = document.querySelector('#formulario');
+const ul = document.querySelector('.listas-de-ideas');
+const inputIdea = document.querySelector('#input-idea');
+const descriptionIdea = document.querySelector('#idea-description');
+
+/* 
+-- TODO:
+[] EVENT LISTENER
+[] START INPUT -> con Enter key o form submit
+[] CREADOR DIV (dentro del ul)
+[] CREADOR LI (dentro de div)
+[] CREADOR LISTA -> 
+
+ */
